bcee89260220511cc50c003060aff3f3616d4040
Niru Hirapara <niruhirapara@Nirus-MacBook-Pro.local>
2021-12-06 18:23:31 -0600

    initialize project

---
+++ src/test/java/com/testNg/testNgAnnotations.java
@@ -0,0 +1,92 @@
+package com.testNg;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class testNgAnnotations {
+
+	@Test
+	public void testcase1() {
+		System.out.println("inside testcase1");
+
+	}
+
+	@Test
+	public void testcase2() {
+		System.out.println("inside testcase2");
+
+	}
+
+	@Test
+	public void testcase3() {
+		System.out.println("inside testcase3");
+
+	}
+
+	@Test
+	public void Testcase41() {
+		System.out.println("inside testcase4");
+
+	}
+
+	@Test
+	public void testcase5() {
+		System.out.println("inside testcase5");
+
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("inside beforeMethod");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("inside afterMethod");
+
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("inside beforeClass");
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		System.out.println("inside afterClass");
+
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("inside beforeTest");
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("inside afterTest");
+
+	}
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("inside beforeSuite");
+
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("inside afterSuite");
+
+	}
+
+}
